adcb0b08e8446b4b338ce65f6db37cad3771c605
Ophiuchus <Jacob Ingalls>
2020-11-20 16:39:56 -0700

    minor fixes

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import numpy as np
 import sys
 import cv2
 from faceMap import setFaces 
-import rubik-solver
+from rubik_solver import utils
 
 images = []
 videos = []
@@ -16,19 +16,19 @@ for i in sys.argv[1:]:
 # colors are ints 0-5, these correspond to Yellow, Red, Green, Orange, Blue, and White
 
 class Cube:
-    def __init__():
+    def __init__(self):
         print("TODO: Cube()")
-        faces = 0
-        notatedList = [-1 for i in range(54)]
+        self.faces = 0
+        self.notatedList = [-1 for i in range(54)]
         
-    def solution():
+    def solution(self):
         print("TODO: Cube.solution()")
 
-    def addFace(): #faces are a 3x3 list of ints
-        self.faces += faceMap
+    def addFace(self, facesData): #faces are a 3x3 list of ints
+        self.faces += faceMap(self.notatedList, facesData)
 
-    def completeCube():
-        return faces == 6
+    def completeCube(self):
+        return self.faces == 6
         print("TODO: Cube.completeCube()")
 
 
@@ -39,14 +39,14 @@ images = [cv2.imread(i) for i in images]
 print("TODO: process images")
 
 if cube.completeCube():
-    print cube.solution()
+    print(cube.solution())
     exit()
 
 for v in videos:
     print("TODO: process videos")
 
 if cube.completeCube():
-    print cube.solution()
+    print(cube.solution())
     exit()
 
 print("Sorry, not enough information")
