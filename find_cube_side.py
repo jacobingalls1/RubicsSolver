e6cb15b875db48436616d46eec5a0107a18bd9c4
RuidiH <ruidihuang@mymail.mines.edu>
2020-12-13 16:12:31 -0700

    adjusted find_cube_side threshold

--- find_cube_side.py
+++ find_cube_side.py
@@ -136,7 +136,7 @@ def find_sides(image=cv2.imread("testing/L1.jpg"), demo=False):
     for n in range(num_labels):
         xc, yc = centroids[n]
         area = stats[n, cv2.CC_STAT_AREA]
-        if area > 500 and area < 8000:   # was 50 and 8000 for L1
+        if area > 50 and area < 8000:   # was 50 and 8000 for L1
             centroid_location_white.append((xc, yc, area))
 
     # reverse black and white and find Black centroids
@@ -217,4 +217,4 @@ def find_sides(image=cv2.imread("testing/L1.jpg"), demo=False):
         faces[f] = [Sticker(findColor(bgr_img, i), i) for i in faces[f]]
     return faces
 
-find_sides(image=cv2.imread("testing/m1b.jpg"), demo=True)
+find_sides(image=cv2.imread("testing/L1.jpg"), demo=True)
